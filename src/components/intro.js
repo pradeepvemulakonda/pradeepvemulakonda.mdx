e729dc87055ad9234a824a34fac14648c79c075f
pradeepvemulakonda@gmail.com <ramani456%>
2019-03-11 23:11:34 +1100

    github button and Intro update

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,36 +1,60 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 import Image from 'gatsby-image';
+import styled from 'styled-components';
+import GitHubButton from 'react-github-btn'
 
 import { rhythm } from '../utils/typography';
 
-function Intro () {
+const Details = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: ${rhythm(1/4)};
+`;
+
+const Name = styled.div`
+  margin: auto;
+`;
+
+const Role = styled.div`
+  margin: auto;
+`;
+
+const Intro = () => {
   return (
     <StaticQuery
       query={introQuery}
       render={data => {
         const { author } = data.site.siteMetadata;
         return (
-          <div
-            style={{
-              display: `flex`,
-              marginBottom: rhythm(2.5)
-            }}
-          >
-            <Image
-              fixed={data.avatar.childImageSharp.fixed}
-              alt={author}
-              style={{
-                marginBottom: 0,
-                borderRadius: `100%`,
-                display: 'block',
-                marginLeft: 'auto',
-	              marginRight: 'auto'
-              }}
-              imgStyle={{
-                borderRadius: `50%`
-              }}
-            />
+          <div style={{
+            display: `flex`,
+            flexDirection: 'column',
+            marginBottom: rhythm(1.5)
+          }}>
+            <div>
+              <Image
+                fixed={data.avatar.childImageSharp.fixed}
+                alt={author}
+                style={{
+                  marginBottom: 0,
+                  borderRadius: `100%`,
+                  display: 'block',
+                  marginLeft: 'auto',
+                  marginRight: 'auto'
+                }}
+                imgStyle={{
+                  borderRadius: `50%`
+                }}
+              />
+            </div>
+            <Details>
+                <Name>Bhaskara Pradeep Vemulakonda</Name>
+                <Role>Senior Solutions Designer at Westpac</Role>
+                <GitHubButton href="https://github.com/pradeepvemulakonda" data-size="large" aria-label="Follow @pradeepvemulakonda on GitHub">
+                  Follow @pradeepvemulakonda
+                </GitHubButton>
+            </Details>
           </div>
         );
       }}
