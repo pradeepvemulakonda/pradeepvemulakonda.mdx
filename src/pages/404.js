d698b02cbcc798ba57e0dd6163d9641b1ea91748
pradeepvemulakonda <pradeepvemulakonda@gmail.com>
2019-03-17 11:25:20 +1100

    added java tutorial

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,51 @@
 import React from 'react'
-
+import Image from 'gatsby-image';
 import Layout from '../layout/primary-layout'
 import SEO from '../components/seo'
+import { StaticQuery, graphql, Link } from 'gatsby';
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+  <StaticQuery
+      query={NotFoundQuery}
+      render={data => {
+        const { image } = data.notFoundImage.childImageSharp.fixed;
+        return (
+          <Layout>
+            <SEO title="404: Not found" />
+            <Image
+                fixed={image}
+                alt='NOT FOUND'
+                style={{
+                  marginBottom: 0,
+                  borderRadius: `100%`,
+                  display: 'block',
+                  marginLeft: 'auto',
+                  marginRight: 'auto'
+                }}
+              />
+            <h1>NOT FOUND</h1>
+            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+            <Link to='/'>
+              <button>Home</button>     
+            </Link>
+            <p>Photo by NASA on Unsplash</p>
+          </Layout>
+        )
+      }
+    }
+   />
+);
+
+const NotFoundQuery = graphql`
+  query NotFoundQuery {
+    notFoundImage: file(absolutePath: { regex: "/404.jpg/" }) {
+      childImageSharp {
+        fixed(width: 600, height: 400) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`;
 
 export default NotFoundPage
