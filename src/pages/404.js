37e739d3912a036829f31ebf4746b6d630e3ef25
pradeepvemulakonda@gmail.com <ramani456%>
2019-03-17 17:52:05 +1100

    home

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,9 @@ import React from 'react'
 import Image from 'gatsby-image';
 import Layout from '../layout/primary-layout'
 import SEO from '../components/seo'
-import { StaticQuery, graphql, Link } from 'gatsby';
+import { StaticQuery, graphql, navigate } from 'gatsby';
+import Button from '@material-ui/core/Button';
+import HomeIcon from '@material-ui/icons/Home';
 
 const NotFoundPage = () => (
   <StaticQuery
@@ -14,16 +16,30 @@ const NotFoundPage = () => (
         return (
           <Layout>
             <SEO title="404: Not found" />
-            <Image
+            <figure>
+              <Image
                 fluid={image}
                 alt='NOT FOUND'
               />
+              <figcaption>Photo by NASA on Unsplash</figcaption>
+            </figure>
             <h1>NOT FOUND</h1>
             <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-            <Link to='/'>
-              <button>Home</button>     
-            </Link>
-            <p>Photo by NASA on Unsplash</p>
+              <Button
+                aria-label='Home'
+                aria-haspopup='false'
+                variant='contained'
+                color='primary'
+                onClick={() => {
+                  navigate(
+                    '/',
+                    { replace: false }
+                  );
+                }}
+              >
+                Home
+                <HomeIcon /> 
+              </Button>   
           </Layout>
         )
       }
