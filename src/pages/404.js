92bfc6a177bd3964fbc28067e3e3237dac01cff3
pradeepvemulakonda@gmail.com <ramani456%>
2019-03-17 22:11:26 +1100

    removed unused code

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,50 +1,49 @@
-import React from 'react'
+import React from 'react';
 import Image from 'gatsby-image';
-import Layout from '../layout/primary-layout'
-import SEO from '../components/seo'
+import Layout from '../layout/primary-layout';
+import SEO from '../components/seo';
 import { StaticQuery, graphql, navigate } from 'gatsby';
 import Button from '@material-ui/core/Button';
 import HomeIcon from '@material-ui/icons/Home';
 
 const NotFoundPage = () => (
   <StaticQuery
-      query={NotFoundQuery}
-      render={data => {
-        debugger;
-        console.log('==>', data.notFoundImage.childImageSharp.fluid);
-        const image  = data.notFoundImage.childImageSharp.fluid;
-        return (
-          <Layout>
-            <SEO title="404: Not found" />
-            <figure>
-              <Image
-                fluid={image}
-                alt='NOT FOUND'
-              />
-              <figcaption>Photo by NASA on Unsplash</figcaption>
-            </figure>
-            <h1>NOT FOUND</h1>
-            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-              <Button
-                aria-label='Home'
-                aria-haspopup='false'
-                variant='contained'
-                color='primary'
-                onClick={() => {
-                  navigate(
-                    '/',
-                    { replace: false }
-                  );
-                }}
-              >
+    query={NotFoundQuery}
+    render={data => {
+      console.log('==>', data.notFoundImage.childImageSharp.fluid);
+      const image = data.notFoundImage.childImageSharp.fluid;
+      return (
+        <Layout>
+          <SEO title='404: Not found' />
+          <figure>
+            <Image
+              fluid={image}
+              alt='NOT FOUND'
+            />
+            <figcaption>Photo by NASA on Unsplash</figcaption>
+          </figure>
+          <h1>NOT FOUND</h1>
+          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+          <Button
+            aria-label='Home'
+            aria-haspopup='false'
+            variant='contained'
+            color='primary'
+            onClick={() => {
+              navigate(
+                '/',
+                { replace: false }
+              );
+            }}
+          >
                 Home
-                <HomeIcon /> 
-              </Button>   
-          </Layout>
-        )
-      }
+            <HomeIcon />
+          </Button>
+        </Layout>
+      );
     }
-   />
+    }
+  />
 );
 
 const NotFoundQuery = graphql`
@@ -59,4 +58,4 @@ const NotFoundQuery = graphql`
   }
 `;
 
-export default NotFoundPage
+export default NotFoundPage;
