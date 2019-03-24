73b91be3bd950508ddc244f641c20f8ed648d89e
pradeepvemulakonda@gmail.com <ramani456%>
2019-03-24 15:48:12 +1100

    responsive table

--- src/components/responsive-table.js
+++ src/components/responsive-table.js
@@ -1,30 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export const Pre = styled.pre`
-  text-align: left;
-  margin: 1em 0;
-  padding: 0.5em;
-
-  & .token-line {
-    line-height: 1.3em;
-    height: 1.3em;
-  }
-`;
-
-export const LineNo = styled.span`
-  display: inline-block;
-  width: 2em;
-  user-select: none;
-  opacity: 0.3;
+export const OverflowWrapper = styled.div`
+    overflow-x: auto;
 `;
 
 export default class ResponsiveTable extends React.Component {
   render () {
     return (
-      <div class="hello">
+      <OverflowWrapper>
         {this.props.children}
-      </div>
+      </OverflowWrapper>
     );
   }
 }
