db09865667bc2014dc5ed8e5131205bc34e3a7d5
pradeepvemulakonda@gmail.com <ramani456%>
2019-04-04 22:24:42 +1100

    moved yaml files to content

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/data`,
+        path: `${__dirname}/content/data`,
         name: `data`
       }
     },
